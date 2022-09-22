0a86725ac220c352e5a57675dd47391e62fcbbc6
Radu Enuca <raduenuca@microsoft.com>
2022-09-22 08:28:50 +0300

    chore: add *.cjs in ignorePatters for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  ignorePatterns: ['**/dist/**'],
+  ignorePatterns: ['**/dist/**', '**/*.cjs'],
   rules: {
     '@typescript-eslint/no-empty-interface': [
       'error',
